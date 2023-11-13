e0cf311dea5872de904a9842d304210d6828f7b8
Arasamannar A K <arasamannar@gmail.com>
2023-11-13 05:53:12 +0530

    added Nav bar and head section of the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Nav from './Nav';
+import Header from './Header';
 
 function App() {
+const cartDetails = [
+
+  {
+    
+  }
+
+]
+
   return (
     <div>
-      Hello
+      <Nav></Nav>
+      <Header></Header>
     </div>
   );
 }
